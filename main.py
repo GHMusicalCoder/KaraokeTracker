49b0faf80a95758fc303292692ad4a52b061f960
GHMusicalCoder <github@musicalcoder.com>
2018-04-23 01:56:54 -0400

    move 200 cdg/mp3 files into temp folder

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import shutil
 import zipfile
 
 # main variables
+stop_count = 200
+file_count = 0
 t_folder = 'KT_Temp'
 f_folder = 'Final_Karaoke'
 s_folder = 'KaraokeSCCDs'
 temp = os.path.abspath(os.path.join('/', 'home', 'chris', 'Temp', t_folder))
 final = os.path.abspath(os.path.join('/', 'home', 'chris', 'Work', f_folder))
 work = os.path.abspath(os.path.join('/', 'home', 'chris', 'Work', s_folder))
-stop_count = 100
-file_count = 0
+extraneous = []
 
 
 def test_paths():
@@ -26,73 +27,76 @@ def test_paths():
     return True
 
 
+def count_current_items():
+    global file_count
+    for _ in os.listdir(temp):
+        file_count += 1
+
+
 def log_info(s):
     print('<', '*' * 58, '>')
     print(s)
     print('<', '*' * 58, '>')
 
 
-def file_count_check():
+def is_under_file_count():
+    global file_count
     if file_count < stop_count:
-        file_count += 1
+        file_count += 2
         return True
     return False
 
 
-def move_cdg(cdg_list):
+def proc_folder(folder):
+    for _ in os.listdir(folder):
+        entry = os.path.join(folder, _)
+        if os.path.isdir(entry):
+            proc_folder(entry)
+        else:
+            proc_file(entry)
+
+
+def proc_file(full_file):
+    path = os.path.dirname(full_file)
+    file = os.path.basename(full_file)
+    name, ext = os.path.splitext(file)
+    if ext.lower() == ".zip":
+        proc_zip(full_file)
+    elif ext == ".CDG":
+        fix_cdg(path, name)
+    elif ext == ".cdg":
+        proc_cdg(path, name)
+    else:
+        extraneous.append(full_file)
+
+
+def proc_zip(zip):
     pass
 
 
-def move_zip(zip_list):
-    pass
+def fix_cdg(path, name):
+    os.rename("{0}/{1}.CDG".format(path, name), "{0}/{1}.cdg".format(path, name))
+    # test mp3 portion
+    if os.path.isfile("{0}/{1}.MP3".format(path, name)):
+        os.rename("{0}/{1}.MP3".format(path, name), "{0}/{1}.mp3".format(path, name))
+    proc_cdg(path, name)
+
+
+def proc_cdg(path, file):
+    if is_under_file_count():
+        shutil.move("{0}/{1}.cdg".format(path, file), "{0}/{1}.cdg".format(temp, file))
+        shutil.move("{0}/{1}.mp3".format(path, file), "{0}/{1}.mp3".format(temp, file))
 
 
 def main():
     if not test_paths():
         exit()
 
-    folder_list = []
-    zip_list = []
-    file_list = []
-    bad_entries = []
-
-    for entry in os.listdir(work):
-        _ = os.path.join(work, entry)
-        if os.path.isdir(_):
-            folder_list.append(entry)
-        else:
-            file_name, file_ext = os.path.splitext(entry)
-            if file_ext == '.zip':
-                zip_list.append(file_name)
-            else:
-                if file_ext == '.cdg':
-                    # cdg should have a corresponding mp3 file - if so - we have a matched pair so file name is good
-                    if os.path.isfile(file_name + '.mp3'):
-                        file_list.append(file_name)
-                    else:
-                        bad_entries.append(entry)
-                else:
-                    if not file_ext == '.mp3':
-                        bad_entries.append(entry)
-
-    # process cdg files
-    if file_list:
-        move_cdg(file_list)
-    if file_count_check() and zip_list:
-        move_zip(zip_list)
-    log_info('Folders: \n' + '\n'.join(folder_list))
-    log_info('Zips: \n' + '\n'.join(zip_list))
-    log_info('CDGs: \n' + '\n'.join(file_list))
-    log_info('Bad Entries: \n' + '\n'.join(bad_entries))
-
-    # while file_number < 52:
-    #     the_path = dir_start + str(file_number) + dir_end
-    #     process(the_path, file_number)
-    #     if file_number == 43:
-    #         the_path = dir_start + str(file_number) + ' Missing CDG Files'
-    #         process(the_path, file_number)
-    #     shutil.rmtree(the_path)
-    #     file_number += 1
+    count_current_items()
+    proc_folder(work)
+    if file_count > 0:
+        # start processing temp folder
+        pass
 
 
 if __name__ == '__main__':
