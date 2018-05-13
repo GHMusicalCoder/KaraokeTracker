edeb4f6846a2addb849cd0c2844cd884fd80df40
GHMusicalCoder <github@musicalcoder.com>
2018-05-13 14:57:54 -0400

    added db update #1 function to db class

--- data.py
+++ data.py
@@ -1,5 +1,4 @@
 import sqlite3
-import sys
 from contextlib import contextmanager
 
 
@@ -21,9 +20,7 @@ class TrackingDB(object):
         self.db_valid = True
         if first_run:
             # the database doesn't exist - so we will initiate the file and add the kt_main table
-            print("*" * 60)
-            print("Initializing the database file")
-            print()
+            self.print_class_message("Initializing the database file")
             try:
                 conn = sqlite3.connect(db_file)
                 cursor = conn.cursor()
@@ -35,6 +32,7 @@ class TrackingDB(object):
             except Exception as e:
                 self.db_valid = False
         else:
+            self.print_class_message("Checking for database updates...")
             conn = sqlite3.connect(db_file)
             cursor = conn.cursor()
             cursor.execute("SELECT version FROM kt_main;")
@@ -43,6 +41,13 @@ class TrackingDB(object):
             conn.close()
 
         self.db_valid = self.db_update_all()
+
+    def print_class_message(self, message, close=True, open=True):
+        if open:
+            print("*" * 80)
+        print(message)
+        if close:
+            print()
 
     def db_update_all(self):
         """
@@ -56,91 +61,79 @@ class TrackingDB(object):
         :return: True if no issues, otherwise false
         """
 
-        if self.db_version > 1:
+        if self.db_version < 1:
             # run DB update 1
             if not self.db_update_one():
                 return False
 
-        if self.db_version == 1:
+        # add new update if here - if self.db_version < 2:
+        if self.db_version == 1:    # change this to the new update # when making a new update
             return True
 
     def db_update_one(self):
         """
-        first database update
+        first database update - creating the following tables:
+        * Artists - the table of artists
+        * ArtistAlias - the table of misspelled artists and the associated artist
+        * Song - the table of songs
+        * SongFeat - additional artists associated with song (Uptown Funk - Mark Ronson ft Bruno Mars)
+        * KaraokeVendors - table of karaoke vendors with primary disc abbreviation
+        * KaraokeAbbreviations - table of karaoke vendor disc abbreviations
         :return: True if no problem with update - otherwise returns false
         """
-        return True
+        try:
+            self.print_class_message("Applying Database Update #1...", close=False)
+            with self.access_db() as c:
+                print("   Creating Artist Table...")
+                c.execute("CREATE TABLE if not exists Artists (ArtistID INTEGER PRIMARY KEY, Artist TEXT);")
+                print("   Creating Artist Alias table...")
+                c.execute("CREATE TABLE if not exists ArtistAlias "
+                          "(ArtistAliasID INTEGER PRIMARY KEY, ArtistAlias TEXT, ArtistID INTEGER,"
+                          "FOREIGN KEY(ArtistID) REFERENCES Artists(ArtistID));")
+                print("    Creating Song table...")
+                c.execute("CREATE TABLE if not exists Song "
+                          "(SongID INTEGER PRIMARY KEY, Song TEXT, PrimaryArtist INTEGER,"
+                          "IsMusic INTEGER, IsKaraoke INTEGER,"
+                          "FOREIGN KEY(PrimaryArtist) REFERENCES Artists(ArtistID));")
+                print("    Creating Song Featuring table...")
+                c.execute("CREATE TABLE if not exists SongFeaturing"
+                          "(SongID INTEGER, ArtistID INTEGER, PRIMARY KEY (SongID, ArtistID),"
+                          "FOREIGN KEY(ArtistID) REFERENCES Artists(ArtistID),"
+                          "FOREIGN KEY(SongID) REFERENCES Song(SongID));")
+                print("    Creating Karaoke Vendors table...")
+                c.execute("CREATE TABLE if not exists KaraokeVendors"
+                          "(VendorID INTEGER PRIMARY KEY, Vendor TEXT);")
+                print("    Creating Karaoke Abbreviations table...")
+                c.execute("CREATE TABLE if not exists VendorAbbreviations"
+                          "(VendorID INTEGER, Abbreviation TEXT UNIQUE, IsPrimaryAbbreviation INTEGER,"
+                          "FOREIGN KEY(VendorID) REFERENCES KaraokeVendors(VendorID));")
+                print("    Creating Song Alias table...")
+                c.execute("CREATE TABLE if not exists SongAlias"
+                          "(SongAliasID INTEGER PRIMARY KEY, SongID INTEGER, SongAlias TEXT,"
+                          "FOREIGH KEY(SongID) REFERENCES Song(SongID));")
+                print("    Creating Karaoke Disc table...")
+                c.execute("CREATE TABLE if not exists KaraokeDiscs"
+                          "(DiscID INTEGER PRIMARY KEY, VendorID INTEGER, Disc TEXT,"
+                          "FOREIGN KEY(VendorID) REFERENCES KaraokeVendors(VendorID));")
+                print("    Creating Karaoke Track table...")
+                c.execute("CREATE TABLE if not exists KaraokeTracks"
+                          "(TrackID INTEGER PRIMARY KEY, SongID INTEGER, DiscID INTEGER,"
+                          "TrackNo INTEGER, HasVocal INTEGER, FOREIGN KEY(SongID) REFERENCES Song(SongID),"
+                          "FOREIGN KEY(DiscID) REFERENCES KaraokeDiscs(DiscID));")
+                c.execute("UPDATE kt_main SET version = 1;")
+                self.print_class_message("All #1 updates have been applied successfully", open=False)
+            return True
+        except Exception as e:
+            return False
 
-def db_first_launch(DB):
-    try:
-        conn = sqlite3.connect(DB)
-    except:
-        sys.exit('Error Code DB-X for ' + DB)
-
-
-@contextmanager
-def access_db(DB):
-    try:
-        conn = sqlite3.connect(DB)
-        cursor = conn.cursor()
-        yield cursor
-    finally:
-        conn.commit()
-        conn.close()
-
-
-def make_tables(DB):
-    with access_db(DB) as cursor:
-        # Turn On Foreign Keys
-
-        # Artist table - ID and Artist Name (maybe additional items later)
-        cursor.execute("CREATE TABLE if not exists Artists (ArtistID INTEGER PRIMARY KEY, Artist TEXT);")
-        # Temp Artist table - this may or may not be used - depending on how I work this program
-        cursor.execute("CREATE TABLE if not exists TempArtists (TempID INTEGER, Name TEXT);")
-        # Artist Link - this is for bad spellings of name (Bily Jeol vs Billy Joel) - to automate repair for songs
-        cursor.execute(
-            """CREATE TABLE if not exists ArtistLink 
-                (LinkID INTEGER PRIMARY KEY, Link TEXT, ArtistID INTEGER, 
-                FOREIGN KEY(ArtistID) REFERENCES Artists(ArtistID));""")
-        # Song Table - ID, Name, PrimaryArtist (link to Artist), IsMusic (for later linking to DJ MP3s)
-        # -- IsKaraoke (to separate Karaoke vs DJ MP3)
-        cursor.execute(
-            """CREATE TABLE if not exists Song 
-                (SongID INTEGER PRIMARY KEY, Song TEXT, PrimaryArtist INTEGER,
-                IsMusic INTEGER, IsKaraoke INTEGER, FOREIGN KEY(PrimaryArtist) REFERENCES Artists(ArtistID));""")
-        # SongFeaturing - Links Songs with Featuring Artists (ie Love the Way You Lie - Eminem w/ Rhianna)
-        cursor.execute(
-            """CREATE TABLE if not exists SongFeaturing 
-                (SongID INTEGER, ArtistID INTEGER, PRIMARY KEY (SongID, ArtistID),
-                FOREIGN KEY(ArtistID) REFERENCES Artists(ArtistID),
-                FOREIGN KEY(SongID) REFERENCES Song(SongID));""")
-        # KaraokeVendors - Main Karaoke Track Vendors and their Primary Abbreviation for TrackLinks
-        cursor.execute(
-            """CREATE TABLE if not exists KaraokeVendors 
-                (VendorID INTEGER PRIMARY KEY, Vendor TEXT, PrimaryAbbreviation TEXT);""")
-        # KaraokeAbbreviations - Additional Abbreviations (Like SunFly has SF and SFGG or SFKK)
-        cursor.execute(
-            """CREATE TABLE if not exists VendorAbbreviations 
-                (VendorID INTEGER, Abbreviation TEXT UNIQUE,
-                FOREIGN KEY(VendorID) REFERENCES KaraokeVendors(VendorID));""")
-        # KaraokeDisks - The individual Vendor Discs
-        cursor.execute(
-            """CREATE TABLE if not exists KaraokeDisks 
-                (DiscID INTEGER PRIMARY KEY, VendorID INTEGER, Disc TEXT,
-                FOREIGN KEY(VendorID) REFERENCES KaraokeVendors(VendorID));""")
-        # KaraokeTracks - The individual Tracks - links Song to Karaoke Disc
-        cursor.execute(
-            """CREATE TABLE if not exists KaraokeTracks
-                (TrackID INTEGER PRIMARY KEY, SongID INTEGER, DiscID INTEGER, TrackNo INTEGER, HasVocal INTEGER,
-                FOREIGN KEY(SongID) REFERENCES Song(SongID), FOREIGN KEY(DiscID) REFERENCES KaraokeDiscs(DiscID));""")
-        # At this point - we can only add NEW Tables above - (as we have added data) - changes to existing tables
-        # require us to use ALTER TABLE
-
-    print("DB Tables have been created/verified.")
+    @contextmanager
+    def access_db(self):
+        try:
+            conn = sqlite3.connect(self.database)
+            cursor = conn.cursor()
+            yield cursor
+        finally:
+            conn.commit()
+            conn.close()
 
 
-def has_artist(DB, artist):
-    with access_db(DB) as c:
-        c.execute("SELECT * FROM Artist WHERE Artists = {0}".format(artist))
-        artist_exists = c.fetchone()
-        return artist_exists
